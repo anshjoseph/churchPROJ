70560a605bcd9a7d2b5853d6711c58e05822b428
R ansh joseph <ranshjosrph@gmail.com>
2022-07-05 14:25:57 +0530

    basic model creation is done

--- directory/admin.py
+++ directory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+# importing model form ./models.py
+from .models import Ward,Family,People
+
+# Register models
+admin.site.register(Ward)    # WARD model
+admin.site.register(Family)  # FAMILY model
+admin.site.register(People)  # PEOPLE model
--- directory/models.py
+++ directory/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+# for signaling in future
+from django.dispatch import receiver
+from django.db.models.signals import post_save,post_delete
+# for uniqe id
+from uuid import uuid4
 
-# Create your models here.
+# WARD
+class Ward(models.Model):
+    wardname = models.CharField(max_length=1024)
+    wardhead = models.CharField(max_length=100)
+    # for printing in dashboard
+    def __str__(self) -> str:
+        return f"{self.id}-{self.wardname}"
+    # for printing in terminal
+    def __repr__(self) -> str:
+        return f"{self.wardname}"
+
+# FAMILY
+class Family(models.Model):
+    wardid = models.ForeignKey(Ward,on_delete=models.CASCADE)
+    familyname = models.CharField(max_length=300)
+    menbercount = models.IntegerField(default=0)
+    # for printing in dashboard
+    def __str__(self) -> str:
+        return f"{self.id}-{self.familyname}"
+    # for printing in terminal
+    def __repr__(self) -> str:
+        return f"{self.familyname}"
+
+# PEOPLE
+class People(models.Model):
+    familyid = models.ForeignKey(Family,on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    # feilds were add according to need
+    # for printing in dashboard
+    def __str__(self) -> str:
+        return f"{self.id}-{self.name}"
+    # for printing in terminal
+    def __repr__(self) -> str:
+        return f"{self.name}"
