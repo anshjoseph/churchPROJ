36e06402f81f48142368da91fda43a6277f13285
R ansh joseph <ranshjosrph@gmail.com>
2022-07-05 15:13:03 +0530

    basic serializer for directory

---
+++ directory/modelSerializer.py
@@ -0,0 +1,23 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Ward,Family,People
+
+
+# WARD SERIALIZER
+class SerialWard(ModelSerializer):
+    class Meta:
+        model = Ward
+        fields = ["id","wardname","wardhead"]
+
+
+# FAMILY SERIALIZER
+class SerialFamily(ModelSerializer):
+    class Meta:
+        model = Family
+        fields = ["id","familyname","membercount"]
+
+
+# PEOPLE SERIALIZER
+class SerialPeople(ModelSerializer):
+    class Meta:
+        model = People
+        fields = ["id","name","age"]
