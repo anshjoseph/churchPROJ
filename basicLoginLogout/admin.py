aa95e76713f98bb649d093cf14a0e2be36087dc2
R ansh joseph <ranshjosrph@gmail.com>
2022-07-07 08:29:48 +0530

    session table and master login table created for auth sys

--- basicLoginLogout/admin.py
+++ basicLoginLogout/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import (
+    Session,
+    LoginMaster
+)
 # Register your models here.
+
+
+admin.site.register(Session)
+admin.site.register(LoginMaster)
